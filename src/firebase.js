0983fe34211caab8633c3e387e9288ff3e0ed6b6
futmanagerproschool-pixel <futmanagerproschool@gmail.com>
2026-01-11 10:08:49 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Reemplaza esto con TUS credenciales de la consola de Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyD8COWL_GU3k1oIN37r5rroBuqYvCD4Skw",
-  authDomain: "futmanagerpro-42dfd.firebaseapp.com",
-  projectId: "futmanagerpro-42dfd",
-  storageBucket: "futmanagerpro-42dfd.firebasestorage.app",
-  messagingSenderId: "934402477410",
-  appId: "1:934402477410:web:ce63f48cf38179d67c703b",
-  measurementId: "G-WG0D4K2TF5"
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_PROYECTO.firebaseapp.com",
+  projectId: "TU_PROYECTO",
+  storageBucket: "TU_PROYECTO.appspot.com",
+  messagingSenderId: "TU_ID",
+  appId: "TU_ID_APP"
 };
+
+// 1. Inicializamos la App
+const app = initializeApp(firebaseConfig);
+
+// 2. IMPORTANTE: Usamos "export" para que otros archivos puedan usarlo
+export const db = getFirestore(app);
