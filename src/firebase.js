2974da95ecd46cd70fe854da859b7f88b21999d8
futmanagerproschool-pixel <futmanagerproschool@gmail.com>
2026-01-11 09:48:27 -0500

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD8COWL_GU3k1oIN37r5rroBuqYvCD4Skw",
+  authDomain: "futmanagerpro-42dfd.firebaseapp.com",
+  projectId: "futmanagerpro-42dfd",
+  storageBucket: "futmanagerpro-42dfd.firebasestorage.app",
+  messagingSenderId: "934402477410",
+  appId: "1:934402477410:web:ce63f48cf38179d67c703b",
+  measurementId: "G-WG0D4K2TF5"
+};
